bb587c5cd223e0d4d268a7603f01467ee11a8066
Mark Julius Silverio <mark.julius.silverio@accenture.com>
2021-05-14 08:03:33 +0800

    Added Codility

---
+++ MJS-CodilityBatch28-2021/src/main/java/com/accenture/codility/Sixth.java
@@ -0,0 +1,21 @@
+package com.accenture.codility;
+
+public class Sixth {
+
+	public static void main(String[] args) {
+		System.out.println(solution(10,85,30));
+
+	
+	}
+	
+	//COPY YOUR SOLUTION METHOD here without "static"
+	
+	public static int solution(int X, int Y, int D) {
+		//PROBLEM
+		
+		//SOLUTION
+		
+		return ();
+	}
+
+}
---
+++ MJS-CodilityBatch28-2021/src/main/java/com/accenture/codility/Third.java
@@ -0,0 +1,20 @@
+package com.accenture.codility;
+
+public class Third {
+
+	public static void main(String[] args) {
+		System.out.println(solution(10,85,30));
+
+	}
+	
+	//COPY YOUR SOLUTION METHOD here without "static"
+	
+	public static int solution(int X, int Y, int D) {
+		//PROBLEM
+		
+		//SOLUTION
+		
+		return ();
+	}
+
+}
