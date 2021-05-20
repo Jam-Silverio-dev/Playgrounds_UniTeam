52ed054a0e7f407ca951377b232440015efb0407
Silverio, Mark Julius <mark.julius.silverio@accenture.com>
2021-05-20 18:09:05 +0800

    First spring-boot application

---
+++ bars/src/main/java/com/accenture/bars/BarsAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.bars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarsAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarsAssessmentApplication.class, args);
+	}
+
+}
